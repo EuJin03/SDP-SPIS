41050a5581b35e52631de3a9ca54e56bf2fd7464
eugene <eugenetin99@gmail.com>
2022-02-13 20:11:54 +0800

    -update: initial commit

---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ server/index.js
@@ -0,0 +1,34 @@
+import path from "path";
+import { config } from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+config();
+connectDB();
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", " index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server is running in ${process.env.NODE_ENV} mode on ${PORT}`)
+);
